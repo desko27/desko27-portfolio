1a32ecccc3c4a4d8b88478a1d551bdd579b8bc9b
desko27 <desko27@gmail.com>
2016-10-06 11:02:58 +0200

    add babel-loader babel-core babel-preset-es2015

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,19 @@ module.exports = {
   module: {
     loaders: [
       {
+        test: /\.es6$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
         test: /\.scss$/,
         exclude: /node_modules/,
         loader: 'style-loader!css-loader?sourceMap!autoprefixer-loader!sass-loader?sourceMap'
       }
     ]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.es6']
   }
 }
