1ad02448f1e10958b17f5b674489c97895689d19
desko27 <desko27@gmail.com>
2018-05-22 00:43:52 +0200

    Add actual links to the footer

--- layout/Footer.jsx
+++ layout/Footer.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 export default () => (
   <div className="container">
     <nav className="footer-custom layout">
-      <span className="item">
+      <a className="item" href="/#">
         <i className="fas fa-file-pdf" />
         <span className="item-text">Download my CV</span>
-      </span>
-      <span className="item">
+      </a>
+      <a className="item" href="http://github.com/desko27" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-github" />
         <span className="item-text">GitHub</span>
-      </span>
-      <span className="item">
+      </a>
+      <a className="item" href="http://linkedin.com/in/desko27" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-linkedin" />
         <span className="item-text">LinkedIn</span>
-      </span>
-      <span className="item">
+      </a>
+      <a className="item" href="https://twitter.com/desko27" target="_blank" rel="noopener noreferrer">
         <i className="fab fa-twitter" />
         <span className="item-text">Twitter</span>
-      </span>
+      </a>
     </nav>
   </div>
 );
