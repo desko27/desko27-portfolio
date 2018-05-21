a90a90a77390010423fa2c2bfc9d5d908a960e51
desko27 <desko27@gmail.com>
2018-05-22 01:57:58 +0200

    Add more transition effects to footer links

--- layout/Footer.jsx
+++ layout/Footer.jsx
@@ -4,19 +4,27 @@ export default () => (
   <div className="container">
     <nav className="footer-custom layout">
       <a className="item" href="/#">
-        <i className="fas fa-file-pdf" />
+        <span className="icon">
+          <i className="fas fa-file-pdf" />
+        </span>
         <span className="item-text">Download my CV</span>
       </a>
       <a className="item" href="http://github.com/desko27" target="_blank" rel="noopener noreferrer">
-        <i className="fab fa-github" />
+        <span className="icon">
+          <i className="fab fa-github" />
+        </span>
         <span className="item-text">GitHub</span>
       </a>
       <a className="item" href="http://linkedin.com/in/desko27" target="_blank" rel="noopener noreferrer">
-        <i className="fab fa-linkedin" />
+        <span className="icon">
+          <i className="fab fa-linkedin" />
+        </span>
         <span className="item-text">LinkedIn</span>
       </a>
       <a className="item" href="https://twitter.com/desko27" target="_blank" rel="noopener noreferrer">
-        <i className="fab fa-twitter" />
+        <span className="icon">
+          <i className="fab fa-twitter" />
+        </span>
         <span className="item-text">Twitter</span>
       </a>
     </nav>
