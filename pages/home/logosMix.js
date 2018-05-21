a6323470c7763672a0ff3d190bb8d5c195fba980
desko27 <desko27@gmail.com>
2018-05-22 01:24:16 +0200

    Fix some comments in logosMix.js

--- pages/home/logosMix.js
+++ pages/home/logosMix.js
@@ -32,37 +32,34 @@ window.addEventListener('resize', resizeFunction);
 resizeFunction(); // execute it once
 
 const logos = logoSources.map((logoSrc) => {
-  // create a new Sprite that uses the image name that we just generated as its source
+  // create a new Sprite from the logo image
   const logo = PIXI.Sprite.fromImage(`/static/logos-mix/${logoSrc}.png`);
 
   // set the anchor point so the texture is centerd on the sprite
   logo.anchor.set(0.5);
 
-  // set a random scale for the logo - no point them all being the same size!
-  logo.scale.set(0.8 + (Math.random() * 0.3));
-
-  // finally lets set the logo to be at a random position..
+  // set the logo to be at a random position
   logo.x = Math.random() * app.screen.width;
   logo.y = Math.random() * app.screen.height;
 
-  // create some extra properties that will control movement :
-  // create a random direction in radians. This is a number between 0
+  // create extra properties to control movement
+  // random direction in radians, this is a number between 0
   // and PI*2 which is the equivalent of 0 - 360 degrees
   logo.direction = Math.random() * Math.PI * 2;
 
-  // this number will be used to modify the direction of the logo over time
+  // this will be used to modify the direction of the logo over time
   logo.turningSpeed = Math.random() - 0.8;
 
   // create a random speed for the logo between 0 - 2
   logo.speed = 2 + (Math.random() * 2);
 
-  // finally we push the logo into the logos array so it it can be easily accessed later
+  // done, adding to stage
   app.stage.addChild(logo);
   return logo;
 });
 
 app.ticker.add(() => {
-  // iterate through the logos and update their position
+  // iterate through the logos and update their values
   logos.forEach((logo) => {
     logo.direction += logo.turningSpeed * 0.01;
     logo.x += Math.sin(logo.direction) * logo.speed;
