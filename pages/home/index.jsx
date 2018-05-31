e0c5a45de8118db6e6ccc191b9a5709f26dd28f3
desko27 <desko27@gmail.com>
2018-05-31 18:28:32 +0200

    Add stable way to have emoji in positions

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -3,18 +3,27 @@ import positionsList from './positions.list';
 import { shuffle } from './helpers';
 
 const firstPosition = 'Full-Stack Developer';
+const emojiSplitter = '|';
+
+const positionsEmoji = [];
+const positionsText = [];
 
 class Home extends React.Component {
+  state = {
+    currentEmoji: '',
+  }
+
   componentDidMount() {
     // this will not be run by server
 
     // logo mix via canvas
     require('./logosMix');
 
-    // theater positions
+    // init theater for positions
     const theaterJS = require('theaterjs');
     const theater = theaterJS();
 
+    // common function to add each position
     const addPosition = (thePosition, extraTime = false) => {
       theater
         .addScene('position:')
@@ -23,10 +32,33 @@ class Home extends React.Component {
         .addScene(extraTime ? 3500 : 2000);
     };
 
-    theater.addActor('position', { accuracy: 1, speed: 0.92 });
-    addPosition(firstPosition, true); // first one
+    // mix positions list & split emojis/text
     shuffle(positionsList);
-    positionsList.forEach(p => addPosition(p));
+    positionsList.forEach((p) => {
+      const pair = p.split(emojiSplitter);
+      if (pair.length >= 2) {
+        const [emoji, text] = pair;
+        positionsEmoji.push(emoji.trim());
+        positionsText.push(text); // includes the initial space
+      } else {
+        const [text] = pair;
+        positionsEmoji.push('');
+        positionsText.push(text);
+      }
+    });
+
+    // setup theater
+    theater
+      .on('type:start', () => {
+        const i = positionsText.indexOf(theater.getCurrentSpeech());
+        this.setState({ currentEmoji: positionsEmoji[i] || '' });
+      })
+      .on('erase:end', () => {
+        this.setState({ currentEmoji: '' });
+      });
+    theater.addActor('position', { accuracy: 1, speed: 1 });
+    addPosition(firstPosition, true); // first one
+    positionsText.forEach(p => addPosition(p));
     theater.addScene(theater.replay);
   }
 
@@ -46,7 +78,10 @@ class Home extends React.Component {
           <div className="column">
             <div className="headline">
               <h1 className="fullname">Ismael Ramon</h1>
-              <h2 className="position" id="position">{firstPosition}</h2>
+              <h2 className="position">
+                <span className="emoji">{this.state.currentEmoji}</span>
+                <span id="position">{firstPosition}</span>
+              </h2>
             </div>
           </div>
         </div>
